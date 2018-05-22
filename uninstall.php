cf6b517b048b95d6d418ac9e62ad13b65df55b4a
Kevin Carwile <kcarwile@gmail.com>
2018-05-21 17:56:06 -0700

    updated uninstall to work when only framework available is the one bundled with the plugin being deleted.

--- uninstall.php
+++ uninstall.php
@@ -11,22 +11,28 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) or ! WP_UNINSTALL_PLUGIN ) {
 	die( 'Access denied.' );
 }
 
-if ( file_exists( __DIR__ . '/framework/plugin.php' ) )
-{
+include_once __DIR__ . '/plugin.php';
+
+if ( ! class_exists( 'MWPFramework' ) ) {
+	if ( ! file_exists( __DIR__ . '/framework/plugin.php' ) ) {
+		return;
+	}
+	
 	include_once __DIR__ . '/framework/plugin.php';
-	include_once __DIR__ . '/plugin.php';
-
-	/* Get the plugin instance */
-	$plugin = MWP\Boilerplate\Plugin::instance();
-	$plugin->setPath( rtrim( plugin_dir_path( __DIR__ . '/plugin.php' ), '/' ) );
-
-	/**
-	 * Uninstall it
-	 *
-	 * If you overload this method in your plugin, make sure to call
-	 * parent::uninstall() because the mwp application framework performs
-	 * automatic clean up operations such as the removal of your custom
-	 * database tables.
-	 */
-	$plugin->uninstall();
+	do_action( 'mwp_framework_manual_init' );
 }
+
+/* Get the plugin instance */
+$plugin = MWP\Boilerplate\Plugin::instance();
+$plugin->setPath( rtrim( plugin_dir_path( __DIR__ . '/plugin.php' ), '/' ) );
+
+/**
+ * Uninstall it
+ *
+ * If you overload this method in your plugin, make sure to call
+ * parent::uninstall() because the mwp application framework performs
+ * automatic clean up operations such as the removal of your custom
+ * database tables.
+ */
+$plugin->uninstall();
+
